58884f33fe3232979fa39b4c526d0600fc6001f1
Mike Archie <rusty505@yahoo.com>
2017-07-13 10:20:06 -0700

    non-working code with new fonts

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 $(function() {
-    $("#header").load("header.html");
+
+    $("#header").load("header.html"); //, function() {
+        // var menu = document.getElementById('customNavBar');
+        // var origOffsetY = menu.offset().top;
+        //
+        // function scroll() {
+        //     alert("scrolls: " +    $(window).scrollTop());
+        //     if ($(window).scrollTop() >= origOffsetY) {
+        //         menu.addClass('navbar-static-top');
+        //     //    $('.content').addClass('menu-padding');
+        //     } else {
+        //
+        //         menu.removeClass('navbar-static-top');
+        //         //$('.content').removeClass('menu-padding');
+        //     }
+        // }
+        //
+        // document.onscroll = scroll;
+    //});
 });
